cd8f8bd6f13fd349eee6cf262eb4a0174435e35d
Afeef-ir <afeefaaban747@gmail.com>
2026-03-12 22:59:41 +0530

    Fixed the broken code for button in js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const countDisplay = document.getElementById("treat-count");
 button.addEventListener("click",function () {
   count++;
 
-  if (count==1) {
+  if (count===1) {
     button.textContent =  `${count} like!`;
   } else {
     button.textContent =  `${count} likes`;
-        }
-        countDisplay.textContent = count;
+  }
+  countDisplay.textContent = count;
 });
