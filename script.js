323beba24b3dde7afe08b9a5ab7ea02e621dc355
Afeef-ir <afeefaaban747@gmail.com>
2026-03-12 22:49:41 +0530

    Added colors using css and also added a button using js

---
+++ script.js
@@ -0,0 +1,15 @@
+let count = 0;
+const button = document.getElementById("treat-btn");
+const countDisplay = document.getElementById("treat-count");
+
+
+button.addEventListener("click",function () {
+  count++;
+
+  if (count==1) {
+    button.textContent =  `${count} like!`;
+  } else {
+    button.textContent =  `${count} likes`;
+        }
+        countDisplay.textContent = count;
+});
